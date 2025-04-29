c205f7c19f8afc9b79120789bb458dc3213e399c
Cristian Braga <cristihbr@gmail.com>
2025-04-29 16:36:04 -0300

    adicionando metodo de atualizar produto

--- exerciciossb/src/main/java/br/cristian/exercicioss/controllers/ProdutoControllers.java
+++ exerciciossb/src/main/java/br/cristian/exercicioss/controllers/ProdutoControllers.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,6 +42,14 @@ public class ProdutoControllers {
 		@DeleteMapping("{id}")
 		public void deletar(@PathVariable("id")String id) {
 			produtoRepositores.deleteById(id);
+		}
+		
+		@PutMapping("{id}")
+		public void atualizar(@PathVariable("id") String id, 
+				              @RequestBody Produto produto) {
+			produto.setId(id);
+			produtoRepositores.save(produto);
+			
 		}
 }
 
